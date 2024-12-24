e395ad7499d48a8311c5a6baf56be34420f36742
azizpambayun <aziz.pambayun11@gmail.com>
2024-12-25 01:58:29 +0700

    feat: init project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Sesuaikan dengan struktur folder project kamu
+  theme: {
+    extend: {
+      colors: {
+        "videobelajar-green": "#3ECf4C",
+        "videobelajar-light-green": "#E8FDE1",
+        "videobelajar-primary-text": "#222325",
+        "videobelajar-secondary-text": "#747474",
+      },
+      fontFamily: {
+        lato: ["Lato"],
+      },
+    },
+  },
+  plugins: [],
+};
